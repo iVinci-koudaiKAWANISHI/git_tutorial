5180ad50ce53ab4af3fdc93d5c32df4ead5e65b9
iVinciKoudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2023-05-12 11:28:24 +0900

    prob2

---
+++ basic/prob2/prob2.java
@@ -0,0 +1,15 @@
+package basic.prob2;
+public class prob2{
+    public static void main(String[] args) {
+        int a = 5,b = 3;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("a " + "+ " + "b " +  "= " + (a + b));
+        System.out.println("a " + "- " + "b " +  "= " + (a - b));
+        System.out.println("a " + "* " + "b " +  "= " + (a * b));
+        System.out.println("a " + "/ " + "b " +  "= " + (a / b));
+        System.out.println("a " + "% " + "b " +  "= " + (a % b));
+        
+
+    }
+}
