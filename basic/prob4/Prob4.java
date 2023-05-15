cc563fb4f1a9002b302a26a3f4cc7a63fc183841
iVinciKoudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2023-05-15 17:01:08 +0900

    Prob4（インデント修正）

--- basic/prob4/Prob4.java
+++ basic/prob4/Prob4.java
@@ -3,11 +3,9 @@ public class Prob4{
     public static void main(String[] args) {
         int num = (int)(Math.random()*10)+1; 
         System.out.println(num);
-
         int i;
         for(i = 1;i <= num; i++){
             System.out.print("■");
         }
- 
     }
 }
