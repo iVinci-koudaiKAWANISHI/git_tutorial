dbd746fb4131616e48291c8fdf22a5d96b739ece
iVinciKoudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2023-05-15 10:59:54 +0900

    Prob3_16.18（修正）

--- basic/prob3/Prob3_16.java
+++ basic/prob3/Prob3_16.java
@@ -3,7 +3,21 @@ package basic.prob3;
 public class Prob3_16 {
     public static void main(String[] args) {
         int num = (int)(Math.random()*100)+1; 
-        if(num % 2 == 0 && num % 3 != 0){
+        
+        if(num % 6 == 0){
+            System.out.println(num);
+            System.out.println("2と3の公倍数です。");
+        }else if(num % 2 == 0 ){
+            System.out.println(num);
+            System.out.println("2の倍数です。");
+        }else if(num % 3 == 0){
+            System.out.println(num);
+            System.out.println("3の倍数です。");
+        }else{
+            System.out.println(num);
+        }
+
+        /*if(num % 2 == 0 && num % 3 != 0){
             System.out.println("数値 : " + num);
             System.out.println("2倍数です。");
 
@@ -13,9 +27,9 @@ public class Prob3_16 {
         }else if(num % 2 == 0 && num % 3 == 0){
             System.out.println("数値 : " + num);
             System.out.println("2と3の公倍数です。");
-        }else{
-            System.out.println("数値 : " + num);
-        } 
+        }*/
+            
+        
 
         
  
--- basic/prob3/Prob3_18.java
+++ basic/prob3/Prob3_18.java
@@ -2,7 +2,7 @@ package basic.prob3;
 
 public class Prob3_18 {
     public static void main(String[] args) {
-        int num = (int)(Math.random()*35)-10; 
+        int num = (int)(Math.random()*46)-10; 
         if(num >=25 && num < 30){
             System.out.println("摂氏" + num +"度");
             System.out.println("夏日です。");
