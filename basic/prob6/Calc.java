a242d9a406b6bc0c9f5d5ce446715f4020081cc8
iVinciKoudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2023-05-16 15:05:28 +0900

    Prob6_1~3

---
+++ basic/prob6/Calc.java
@@ -0,0 +1,16 @@
+package basic.prob6;
+
+public class Calc {
+     double add(double a,double b){
+        return a + b;
+    }
+    double sub(double a,double b){
+        return a - b;
+    }
+    double mull(double a,double b){
+        return a * b;
+    }
+    double div(double a,double b){
+        return a / b;
+    }
+}
