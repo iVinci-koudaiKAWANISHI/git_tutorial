807bed3ab0165583dd1e3980730141ebbbb00e72
iVinciKoudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2023-05-16 14:09:32 +0900

    Prob4_15（対応漏れの箇所、1行目に0が表示される、半角スペースを入れるなどを修正）

--- basic/prob4/Prob4_15.java
+++ basic/prob4/Prob4_15.java
@@ -2,8 +2,8 @@ package basic.prob4;
 
 public class Prob4_15 {
     public static void main(String[] args){
-        for(int i = 0; i <= 100; i++){
-            System.out.print(i);
+        for(int i = 1; i <= 100; i++){
+            System.out.print(i + " ");
             if(i %10 == 0)
             System.out.println();
         }
