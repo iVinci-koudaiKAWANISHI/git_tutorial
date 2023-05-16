6b4b6386495ea4cb53633d0434c88c1cf813fc51
iVinciKoudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2023-05-16 10:09:20 +0900

    Prob5_1~5

---
+++ basic/prob5/Prob5_2.java
@@ -0,0 +1,10 @@
+package basic.prob5;
+
+public class Prob5_2 {
+    public static void main(String[] args){
+        double[] d = {0.2, -5.1, 3.2, 1.8};
+        for(int i = 0; i < d.length; i++){
+            System.out.print("d[" + i + "]=" +d[i]+" ");
+        }
+    }
+}
---
+++ basic/prob5/Prob5_5.java
@@ -0,0 +1,27 @@
+package basic.prob5;
+
+public class Prob5_5 {
+    public static void main(String[] args){
+        int[] data = new int[10];
+        for(int i = 0; i < data.length; i++){
+            int num = (int)(Math.random() * 10) + 1;
+            data[i] = num;
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+        System.out.println();
+        System.out.print("3の倍数：");
+        for(int i = 0; i < data.length; i++){
+            if(data[i] % 3 == 0){
+                System.out.print(data[i] + " ");
+            }
+        }
+        System.out.println();
+        System.out.print("3の倍数以外の数：");
+        for(int i = 0; i < data.length; i++){
+            if(data[i] % 3 != 0){
+                System.out.print(data[i] + " ");
+            }
+        }
+    }
+}
