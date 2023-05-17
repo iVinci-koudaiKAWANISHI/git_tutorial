d4678c54240b4f61e6260945e62bb12f31f32eb6
iVinciKoudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2023-05-17 10:58:22 +0900

    hard : Prob2_1~2

---
+++ hard/prob2/Fighter.java
@@ -0,0 +1,10 @@
+package hard.prob2;
+
+public class Fighter {
+    public void fly() {
+        System.out.println("飛行する");
+    }
+    public void fight() {
+        System.out.println("戦闘する");
+    }
+}
