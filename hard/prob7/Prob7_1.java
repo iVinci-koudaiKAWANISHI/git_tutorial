c051753a1314af8d419716f046e7d87fe9ad1d88
iVinciKoudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2023-05-22 17:12:05 +0900

    hard_Prob7_1~2

---
+++ hard/prob7/Prob7_1.java
@@ -0,0 +1,13 @@
+package hard.prob7;
+public class Prob7_1{
+    public static void main(String[] args) {
+        try{
+            int a[] = { 0 , 1, 2 };
+            for(int i = 0; i < 4; i++){
+                System.out.println("a[" + i +"]=" + a[i]);
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("配列の範囲を超えています。");
+        }
+    }
+}
