9e768470aa77ff5263b5be171b493a050e0066cd
iVinciKoudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2023-05-25 16:19:26 +0900

    Revert "Merge pull request #3 from createOrganizationPJ/kawanishi_3"
    This reverts commit b6cdecd5f384f754eb015f7909826dfda16367c2, reversing
    changes made to c1571b2f9c668fd720426f988671e48faed9f99e.

--- basic/prob6/Prob6_1.java
+++
@@ -1,9 +0,0 @@
-package basic.prob6;
-public class Prob6_1{
-    public static void main(String[] args){
-        Minmax m = new Minmax();
-        int a = 9,b = 4,c = 8;
-        System.out.println(a + "と" + b + "と" + c +"のうち、最大のものは" + m.max(a,b,c));
-        System.out.println(a + "と" + b + "と" + c +"のうち、最小のものは" + m.min(a,b,c));
-    }
-}
