d13c2fdc2c83bf8de164b232127fdcdff2e14693
iVinciKoudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2023-06-01 15:00:57 +0900

    (修正)

--- hard/prob7/Main.java
+++ hard/prob7/Main.java
@@ -9,18 +9,16 @@ public class Main {
         //当選番号
         int winningNumber = 242358;
         // 年賀状番号
-        ArrayList<Integer> newYearCards = new ArrayList<>(Arrays.asList(295283,267358,242359));
+        ArrayList<Integer> newYearCards = new ArrayList<>(Arrays.asList(295283,262358,242359));
         for(int i = 0; i < newYearCards.size(); i++){
             if(winningNumber == newYearCards.get(i)){
                 System.out.println("一等賞");
-            }else if(winningNumber == newYearCards.get(i) + 1){
+            }else if(winningNumber == newYearCards.get(i) + 1 || winningNumber == newYearCards.get(i) - 1){
                 System.out.println("前後賞");
-            }else if(winningNumber == newYearCards.get(i) - 1){
-                System.out.println("前後賞");
-            }else if((winningNumber - newYearCards.get(i)) % 100 == 0){
-                System.out.println("三等賞");
-            }else if((winningNumber - newYearCards.get(i)) % 100 == 0){
+            }else if((winningNumber - newYearCards.get(i)) % 10000 == 0){
                 System.out.println("二等賞");
+            }else if((winningNumber - newYearCards.get(i)) % 1000 == 0){
+                System.out.println("三等賞");
             }else{
                 System.out.println("外れ");
             }
